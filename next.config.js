823b623f4ed58adf2a296e384cedd40ae9efb765
Yevhen Noskov <evgeny.noskov@gmail.com>
2023-12-13 19:00:15 +0200

    Issue repoduce

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
+const withVanillaExtract = createVanillaExtractPlugin({
+    identifiers: ({ hash }) => `prefix-${hash}` //issue with hyphen!!
+});
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+module.exports = withVanillaExtract(nextConfig);
---
+++ src/components/MyComponent.tsx
@@ -0,0 +1,5 @@
+import { cls } from "./MyComponent.css";
+
+export default function MyComponent(){
+    return <div className={cls}>Hello, world!</div>
+}
